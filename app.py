1f622f9e7b962ef68d0c0d8b936c2633ecc8aa5e
Aryan <agadre@purdue.edu>
2026-01-09 13:32:12 -0500

    updated free response template

--- app.py
+++ app.py
@@ -8,15 +8,32 @@ app = Flask(__name__)
 @app.route('/')
 def home():
 
-    '''url = "https://api.boilerexams.com/questions/1f86e0a7-0736-4f45-85d7-b0e4cdf5f969"
+    #url = "https://api.boilerexams.com/questions/1f86e0a7-0736-4f45-85d7-b0e4cdf5f969"
+    url = "https://api.boilerexams.com/questions/425ef734-29f3-4b4f-9f1e-5c3617849e47"
     #math_string = r"A line $l$ passes through the points $A(1,-2,1)$ and $B(2,3,-1)$. At what point does this line intersect with the $x y$-plane?"
     
     response = requests.get(url)
     data = response.json()
+    qType = data['type']
     math_string = data['data']['body']
-    return render_template('index.html', formula=math_string)'''
+    options = []
+    if qType == 'MULTIPLE_CHOICE':
+        for i in data['data']['answerChoices']:
+            options.append(i['body'])
+        question_data = {
+        "text": math_string,
+        "options": options,
+        "correct_index": data['data']['solution'][0]
+        }
+        print(question_data)
+    else:
+        question_data = {
+        "text": math_string,
+        "answer": data['data']['solution']
+        }
+        print(question_data)
 
-    question_data = {
+    '''question_data = {
         "text": r"A line $l$ passes through $A(1,-2,1)$ and $B(2,3,-1)$. At what point does it intersect the $xy$-plane?",
         "options": [
             r"$(2, 3, 0)$",
@@ -26,9 +43,11 @@ def home():
             r"None of the above" # 5th option example
         ],
         "correct_index": 1  # 0-based index (so 1 is the second option)
-    }
-
-    return render_template('multipleChoice.html', q=question_data)
+    }'''
+    if qType == 'MULTIPLE_CHOICE':
+        return render_template('multipleChoice.html', q=question_data)
+    else:
+        return render_template('freeResponse.html', q=question_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
